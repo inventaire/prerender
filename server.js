af36f7a2536307906dbbec32186f41059744e39e
maxlath <k@maxlath.eu>
2015-07-31 17:30:57 +0200

    using prerender-level-cache

--- server.js
+++ server.js
@@ -17,5 +17,6 @@ server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
 // server.use(prerender.inMemoryHtmlCache());
 // server.use(prerender.s3HtmlCache());
+server.use(require('prerender-level-cache'));
 
 server.start();
