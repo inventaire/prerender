d5081ce61a962ffce51310994e5f2b2845a86b88
maxlath <k@maxlath.eu>
2016-02-17 23:15:35 +0100

    customizing

--- prerender.js
+++ prerender.js
@@ -17,4 +17,16 @@ server.use(prerender.httpHeaders());
 // server.use(prerender.inMemoryHtmlCache());
 // server.use(prerender.s3HtmlCache());
 
+var invPlugins = require('inv-prerender-plugins')
+
+// reading refresh query string parameter
+// before rewriteUrl overrides it
+server.use(invPlugins.forceCacheRefresh);
+// rewrite url before checking cache
+// to get as many cache hit as possible
+server.use(invPlugins.rewriteUrl);
+server.use(invPlugins.multiFreshness);
+
+server.use(require('prerender-level-cache'));
+
 server.start();
