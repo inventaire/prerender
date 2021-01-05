8fcf5ee9490ace3532679dd1cd10e7f5e16f5be3
maxlath <k@maxlath.eu>
2021-01-06 00:19:28 +0100

    config: reduce default ttl to 15 days

--- config/default.js
+++ config/default.js
@@ -2,7 +2,7 @@ module.exports = {
   chromeLocation: '/usr/bin/chromium-browser',
   logBrowserConsole: false,
   logRequests: false,
-  ttl: 2 * 30 * 24 * 60 * 60 * 1000,
+  ttl: 15 * 24 * 60 * 60 * 1000,
   omittedHeaders: [
     'Set-Cookie'
   ]
