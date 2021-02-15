e3001f12580ae614643c00a9d8c85e40ac878ba4
maxlath <k@maxlath.eu>
2021-02-15 22:12:56 +0100

    plugins: transform cleanupUrl into rewriteUrl
    and transform accept-languages header into a lang query parameter
    so that, for instance, a __nojs request with those headers set
    can be served content in their preferred language

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ server.use(prerender.httpHeaders());
 
 // Reading refresh query string parameter before cleanupUrl overrides it
 server.use(prerender.forceCacheRefresh());
-server.use(prerender.cleanupUrl());
+server.use(prerender.rewriteUrl());
 server.use(prerender.removeNoscriptReload());
 
 server.use(require('prerender-level-cache'));
